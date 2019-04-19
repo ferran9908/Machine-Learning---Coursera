17591dcd904d361050b9c2c692a95fa18a3c6e0e
Ferran <ferran.9908@gmail.com>
2019-04-19 16:13:10 +0530

    Finish Ex2

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,11 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+htheta = sigmoid(X*theta);
 
-
-
-
-
+J = (1/m) * (sum(- y' * log(htheta) - (1 - y)' * log(1 - htheta)));
+grad = (1/m) * (htheta - y)' * X;
 
 
 % =============================================================
